91e53c7e340359ea282f7e3be2b4d227566b083c
blue0knight <oni_rivera98@yahoo.com>
2016-06-22 19:18:53 -0400

    generated Rooms controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'rooms/index'
+
+  get 'rooms/show'
+
+  get 'rooms/new'
+
+  get 'rooms/create'
+
+  get 'rooms/edit'
+
+  get 'rooms/update'
+
   root 'pages#home'
 
   devise_for :users,
