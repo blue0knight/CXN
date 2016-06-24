d6bd269dd409066ee9597fcb5ebe16ea1bb78133
blue0knight <oni_rivera98@yahoo.com>
2016-06-24 17:23:59 -0400

    fix error in rooms controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  before_action :set_room, only: [show, edit, update]
+  before_action :set_room, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:show]
 
   def index
