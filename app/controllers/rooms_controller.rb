8c0d93998f64192a1a4ca15e334ca6c4e43ff696
blue0knight <oni_rivera98@yahoo.com>
2016-06-28 23:02:59 -0400

    troubleshooting images not saving to database

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,57 +1,58 @@
 class RoomsController < ApplicationController
-	before_action :set_room, only: [:show, :edit, :update]
-	before_action :authenticate_user!, except: [:show]
+  before_action :set_room, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except: [:show]
 
   def index
-  	@rooms = current_user.rooms
+    @rooms = current_user.rooms
   end
 
   def show
-		@photos = @room.photos
+    @photos = @room.photos
   end
 
   def new
-  	@room = current_user.rooms.build
+    @room = current_user.rooms.build
   end
 
   def create
-  	@room = current_user.rooms.build(room_params)
+    @room = current_user.rooms.build(room_params)
 
-  	if @room.save
-			if params[:images]
-				params[:images].each do |image|
-					@room.photos.create(image: image)
-			end
-		end
+    if @room.save
 
-			@photos = @room.photos
-  		redirect_to edit_room_path(@room), notice: "Saved..."
-  	else
-  		render :new
-  	end
+      if params[:images]
+        params[:images].each do |image|
+          @room.photos.create(image: image)
+        end
+      end
+
+      @photos = @room.photos
+      redirect_to edit_room_path(@room), notice: "Saved..."
+    else
+      render :new
+    end
   end
 
   def edit
-		if current_user.id == @room.user.id
-		@photos = @room.photos
-	  else
-		redirect_to root_path, notice: "You don't have permission."
+    if current_user.id == @room.user.id
+      @photos = @room.photos
+    else
+      redirect_to root_path, notice: "You don't have permission."
     end
   end
 
   def update
-  	if @room.update(room_params)
+    if @room.update(room_params)
 
-			if params[:images]
-				params[:images].each do |image|
-					@room.photos.create(image: image)
-			end
-		end
+      if params[:images]
+        params[:images].each do |image|
+          @room.photos.create(image: image)
+        end
+      end
 
-  		redirect_to edit_room_path(@room), notice: "Updated..."
-  	else
-  		render :edit
-  	end
+      redirect_to edit_room_path(@room), notice: "Updated..."
+    else
+      render :edit
+    end
   end
 
   private
